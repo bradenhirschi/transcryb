b455b794d21c4a84086b94a1b7cabe1b9497d060
Braden Hirschi <braden@cloudpano.com>
2024-07-21 07:45:32 -0700

    Added landing page files to public

--- src/app/api/transcribe/route.ts
+++ src/app/api/transcribe/route.ts
@@ -1,7 +1,17 @@
 import OpenAI from "openai";
 import { NextRequest, NextResponse } from "next/server";
+import createServerComponentClient from "@/utils/server/supabase/client";
+import { UUID } from "crypto";
 
 const openai = new OpenAI();
+
+interface TranscriptionRow {
+  id: UUID;
+  user_email: string;
+  transcription: string;
+  email_sent: boolean;
+  created_at: string;
+}
 
 export async function POST(request: NextRequest) {
   try {
@@ -9,8 +19,20 @@ export async function POST(request: NextRequest) {
     const file = formData.get('file');
 
     if (file === null || typeof file === 'string') {
-      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+      return NextResponse.json({ error: 'Invalid file, cannot transcribe' }, { status: 500 })
     }
+
+    const supabase = createServerComponentClient();
+
+    const transcriptionObj = {
+      user_email: 'bradenhirschi29@gmail.com',
+    }
+
+    const {data, error, status} = await supabase.from('transcriptions').insert(transcriptionObj).select();
+
+    console.log(data);
+
+    // const transcription = {text: 'yee'};
 
     const transcription = await openai.audio.transcriptions.create({
       file: file,
@@ -19,7 +41,7 @@ export async function POST(request: NextRequest) {
   
     return NextResponse.json({ transcription }, { status: 200 });
   } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    console.error('errpr ',error);
+    return NextResponse.json({ error: 'Failed to transcribe audio' }, { status: 500 });
   }
 }
